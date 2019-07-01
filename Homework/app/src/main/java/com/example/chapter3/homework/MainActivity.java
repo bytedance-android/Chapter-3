40865ca5f5044e2e6a776f6d83685d4a4f60c52d
liucheng <liucheng@bytedance.com>
2019-07-02 02:03:10 +0800

    add Homework

---
+++ Homework/app/src/main/java/com/example/chapter3/homework/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.chapter3.homework;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_ex1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Ch3Ex1Activity.class));
+            }
+        });
+        findViewById(R.id.btn_ex2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Ch3Ex2Activity.class));
+            }
+        });
+    }
+}
